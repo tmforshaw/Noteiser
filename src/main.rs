017d767e4fd21b070a4f2234c2e07c509a877202
Tyler Forshaw <tmforshaw@outlook.com>
2022-07-27 17:26:42 +0100

    Errors are working better, abstract code into struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::path::Path;
+
 use clap::{Parser, Subcommand};
 
 // Most linux distros ship with vi
 const EDIT_BIN: &str = "/usr/bin/vi";
-const CONF_DIR: &str = ".config/noteiser/config.toml";
+// const CONF_DIR: &str = ".config/noteiser/config.toml";
 
 // TODO get this from a config file
 const DEV_DIR: &str = "/home/tmforshaw/Development";
@@ -45,46 +47,80 @@ enum RustCommands {
     },
 }
 
-fn run_command(command: &str, args: Vec<&str>) -> Result<std::process::ExitStatus, std::io::Error> {
-    std::process::Command::new(command).args(args).status()
+struct Noteiser {
+    cli: Cli,
+    editor: String,
+}
+
+impl Noteiser {
+    fn new() -> Self {
+        let cli = Cli::parse();
+
+        let editor = if let Some(com_editor) = cli.editor.clone() {
+            com_editor
+        } else if let Ok(env_editor) = std::env::var("EDITOR") {
+            env_editor
+        } else {
+            EDIT_BIN.to_string()
+        };
+
+        Self { cli, editor }
+    }
+
+    fn run_command(
+        command: &str,
+        args: Vec<&str>,
+    ) -> Result<std::process::ExitStatus, std::io::Error> {
+        std::process::Command::new(command).args(args).status()
+    }
+
+    fn run_editor(self: &Self, filepath: &str) -> Result<std::process::ExitStatus, std::io::Error> {
+        if let Ok(path) = Path::new(&filepath).canonicalize() {
+            std::process::Command::new(self.editor.clone())
+                .args(vec![path])
+                .status()
+        } else {
+            Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "Filename was invalid for editor",
+            ))
+        }
+    }
+
+    fn match_command(&self) {
+        match &self.cli.command {
+            Commands::Open { file_name } => {
+                self.run_editor(file_name).unwrap();
+            }
+            Commands::Rust { command } => match &command {
+                RustCommands::New { project_name } => {
+                    let location_string = format!("{DEV_DIR}/Rust/{project_name}");
+                    // let location = Path::new(&location_string.clone());
+
+                    // Check if project already exists
+                    if Self::run_command("ls", vec![location_string.clone().as_str()]).is_err() {
+                        Self::run_command("cargo", vec!["new", location_string.clone().as_str()])
+                            .unwrap();
+
+                        self.run_editor(
+                            format!("{}/src/main.rs", location_string.clone()).as_str(),
+                        )
+                        .unwrap();
+                    } else {
+                        println!("Project already exists");
+                        std::process::exit(0x1000);
+                    }
+                }
+            },
+            Commands::Config => {
+                // Open the config file
+            }
+        }
+    }
 }
 
 fn main() {
-    let cli = Cli::parse();
+    let n = Noteiser::new();
 
-    let editor = if let Some(com_editor) = cli.editor {
-        com_editor
-    } else if let Ok(env_editor) = std::env::var("EDITOR") {
-        env_editor
-    } else {
-        EDITOR_CONST.to_string()
-    };
-
-    match &cli.command {
-        Commands::Open { file_name } => {
-            run_command(editor.as_str(), vec![file_name]).unwrap();
-        }
-        Commands::Rust { command } => match &command {
-            RustCommands::New { project_name } => {
-                let location = format!("{DEV_DIR}/Rust/{project_name}");
-
-                // Check if project already exists
-                if run_command("ls", vec![location.as_str()]).is_err() {
-                    run_command("cargo", vec!["new", location.clone().as_str()]).unwrap();
-
-                    run_command(
-                        editor.as_str(),
-                        vec![format!("{location}/src/main.rs").as_str()],
-                    )
-                    .unwrap();
-                } else {
-                    println!("Project already exists");
-                    std::process::exit(0x1000);
-                }
-            }
-        },
-        Commands::Config => {
-            // Open the config file
-        }
-    }
+    n.match_command();
 }
