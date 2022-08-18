e36cf13d947fd6b088bfe80929b91089c01e1a15
Tyler Forshaw <tmforshaw@outlook.com>
2022-08-18 13:31:55 +0100

    Fix ordering of args in verify_file_and_dir

--- src/temp_control.rs
+++ src/temp_control.rs
@@ -3,14 +3,14 @@ use crate::{get_home, verify_file_and_dir};
 
 use std::fs;
 
-const DEFAULT_FILE_DIR: &str = ".temp/ntsr";
+const DEFAULT_FILE_DIR: &str = ".cache/ntsr";
 const DEFAULT_FILE_NAME: &str = "temp.toml";
 
 #[must_use]
 pub fn get_file() -> fs::File {
     let directory = format!("{}/{DEFAULT_FILE_DIR}", get_home());
 
-    match verify_file_and_dir(directory.as_str(), DEFAULT_FILE_NAME) {
+    match verify_file_and_dir(DEFAULT_FILE_NAME, directory.as_str()) {
         Ok(verified_path) => match fs::File::create(verified_path) {
             Ok(file) => file,
             Err(e) => error!("{e}"),
